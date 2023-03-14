f31049e76d9bd2471ddba0f6a956273c72b820f0
Luca <luca@parsani.it>
2023-03-14 14:00:57 +0100

    Aggiunto il costruttore

--- src/main/java/org/example/Model/Player.java
+++ src/main/java/org/example/Model/Player.java
@@ -1,11 +1,16 @@
 package org.example.Model;
 
 public class Player {
-    //vanno aggiunti i costruttori
-    private int position;
-    private int[] points;
+    private final int position;
+    private int points;
     private Shelf shelf;
     private PersonalGoal personalGoal;
+
+    public Player (int numero) {
+        this.position=numero;
+        this.points=0;
+        //shelf e personalGoal andranno settate con il loro rispettivo set
+    }
 
     public void setPoints(int[] points){
         this.points=points;
