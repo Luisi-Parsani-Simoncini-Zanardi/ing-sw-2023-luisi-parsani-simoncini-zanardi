cf12ab6d8d7b337545332425d4a8559ffc5a147d
Davide Simoncini <simondade@gmail.com>
2023-03-16 01:54:05 +0100

    aggiunti import per PersonalGoal

--- src/main/java/org/projectsw/Model/PersonalGoal.java
+++ src/main/java/org/projectsw/Model/PersonalGoal.java
@@ -1,11 +1,19 @@
 package org.projectsw.Model;
 
+import com.google.gson.Gson;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.IOException;
+
 public class PersonalGoal {
     private Tiles[][] personalGoal;
 
     private static int[] used;//codici già chiamati
 
-    public PersonalGoal(int cardCode){
+    public PersonalGoal(int cardCode) throws IOException {
+
+        Gson gson = new Gson();
+
         personalGoal = new Tiles[6][5];
         for (int i=0;i<6;i++){
             for(int j=0;j<5;j++){
