7a85964c4336d7c67e4e3789aa0779d74e716815
Davide Simoncini <simondade@gmail.com>
2023-03-16 01:48:15 +0100

    completato il costruttore di board da file json (ancora tutto da controllare e testare)

--- src/main/java/org/projectsw/Model/Board.java
+++ src/main/java/org/projectsw/Model/Board.java
@@ -1,12 +1,26 @@
 package org.projectsw.Model;
 
+import com.google.gson.Gson;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Board{
     private Tiles[][] board;
     private boolean endGame;
     private Bag bag;
 
-    public Board(){ //initializes the 4-player board, the controller will eventually reduce it
+    public Board() throws IOException{ //initializes the 4-player board, the controller will eventually reduce it
 
+        Gson gson = new Gson();
+        String[][] tmpMatrix = gson.fromJson(new FileReader("./src/main/resources/PersonalGoals.json"), String[][].class);
+
+        board = new Tiles[9][9];
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(tmpMatrix[i][j].equals("UNUSED")){board[i][j] = Tiles.UNUSED;}
+                if(tmpMatrix[i][j].equals("EMPTY")){board[i][j] = Tiles.EMPTY;}
+            }
+        }
     }
 
     public Board(Board board){
