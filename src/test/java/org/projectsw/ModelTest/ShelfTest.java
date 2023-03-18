8bc3ee9b95ccc42b0a323a73468f317a4e0f635e
Lorenzo Luisi <116544919+Lorenzo-Luisi@users.noreply.github.com>
2023-03-18 17:02:20 +0100

    finished the ShelfTest class

--- src/test/java/org/projectsw/ModelTest/ShelfTest.java
+++ src/test/java/org/projectsw/ModelTest/ShelfTest.java
@@ -17,6 +17,30 @@ class ShelfTest {
     //test that the method insert the correct tile in the correct position in the shelf
     @Test
     void insertTiles() {
+        Shelf shelf = new Shelf();
+        try {
+            shelf.insertTiles(Tiles.CATS, 5,5);
+        }catch(Exception e){
+            assertEquals("Out of bounds", e.getMessage());
+        }
 
+        try {
+            shelf.insertTiles(Tiles.UNUSED, 5,4);
+        }catch(Exception e){
+            assertEquals("You can't add an UNUSED tile to the shelf", e.getMessage());
+        }
+
+        try {
+            shelf.insertTiles(Tiles.EMPTY, 5,4);
+        }catch(Exception e){
+            assertEquals("You can't add an EMPTY tile to the shelf", e.getMessage());
+        }
+
+        assertEquals(Tiles.EMPTY, shelf.getShelf()[5][4]);
+        try {
+            shelf.insertTiles(Tiles.CATS, 5,4);
+        }catch(Exception e){
+        }
+        assertEquals(Tiles.CATS, shelf.getShelf()[5][4]);
     }
 }
