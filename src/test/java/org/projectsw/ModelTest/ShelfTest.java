804595618c50f53c57d3a1c2391097802801ef9a
Lorenzo Luisi <116544919+Lorenzo-Luisi@users.noreply.github.com>
2023-03-18 16:37:34 +0100

    Finished the test class PersonalGoalTest

---
+++ src/test/java/org/projectsw/ModelTest/ShelfTest.java
@@ -0,0 +1,16 @@
+package org.projectsw.ModelTest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShelfTest {
+
+    @Test
+    void getShelf() {
+    }
+
+    @Test
+    void insertTiles() {
+    }
+}
