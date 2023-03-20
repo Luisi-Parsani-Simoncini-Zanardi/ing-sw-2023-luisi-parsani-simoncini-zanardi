84617770de72e05290c4ccac553e20a9a1756b32
Davide Simoncini <simondade@gmail.com>
2023-03-21 00:42:14 +0100

    Added integrityTest in PersonalGoalTest

--- src/test/java/org/projectsw/ModelTest/PersonalGoalTest.java
+++ src/test/java/org/projectsw/ModelTest/PersonalGoalTest.java
@@ -13,14 +13,35 @@ class PersonalGoalTest {
     //test that the method returns the right personalGoal
     @Test
     void getPersonalGoal() throws IOException {
-        PersonalGoal personal = new PersonalGoal(1);
+        PersonalGoal personalGoal = new PersonalGoal(1);
 
         assertTrue(
-        personal.getPersonalGoal()[3][1].equals(Tiles.GAMES)&&
-        personal.getPersonalGoal()[0][0].equals(Tiles.PLANTS)&&
-        personal.getPersonalGoal()[0][2].equals(Tiles.FRAMES)&&
-        personal.getPersonalGoal()[1][4].equals(Tiles.CATS)&&
-        personal.getPersonalGoal()[2][3].equals(Tiles.BOOKS)&&
-        personal.getPersonalGoal()[5][2].equals(Tiles.TROPHIES));
+        personalGoal.getPersonalGoal()[3][1].equals(Tiles.GAMES)&&
+        personalGoal.getPersonalGoal()[0][0].equals(Tiles.PLANTS)&&
+        personalGoal.getPersonalGoal()[0][2].equals(Tiles.FRAMES)&&
+        personalGoal.getPersonalGoal()[1][4].equals(Tiles.CATS)&&
+        personalGoal.getPersonalGoal()[2][3].equals(Tiles.BOOKS)&&
+        personalGoal.getPersonalGoal()[5][2].equals(Tiles.TROPHIES));
+    }
+
+    //test that the constructor returns a correct and full maxtrix of tiles
+    //tested for all 12 codes
+    @Test
+    void integrityTest() throws IOException {
+        for(int i=0;i<12;i++){
+            PersonalGoal personalGoal = new PersonalGoal(i);
+            for(int j=0;j<6;j++){
+                for(int h=0;h<5;h++){
+                    Tiles tile = personalGoal.getPersonalGoal()[j][h];
+                    assertTrue(tile.equals(Tiles.CATS)||
+                                tile.equals(Tiles.FRAMES)||
+                                tile.equals(Tiles.BOOKS)||
+                                tile.equals(Tiles.GAMES)||
+                                tile.equals(Tiles.TROPHIES)||
+                                tile.equals(Tiles.PLANTS)||
+                                tile.equals(Tiles.EMPTY));
+                }
+            }
+        }
     }
 }
