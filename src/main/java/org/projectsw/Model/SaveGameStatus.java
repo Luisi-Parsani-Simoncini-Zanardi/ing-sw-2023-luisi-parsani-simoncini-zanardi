b951e55d0f239ccbcdc0718728f020b46dd4e512
Davide Simoncini <simondade@gmail.com>
2023-03-22 00:06:54 +0100

    Added javadoc to SaveGameStatus

--- src/main/java/org/projectsw/Model/SaveGameStatus.java
+++ src/main/java/org/projectsw/Model/SaveGameStatus.java
@@ -2,22 +2,34 @@ package org.projectsw.Model;
 
 import java.io.FileWriter;
 import java.io.IOException;
-
 import com.google.gson.*;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+/**
+ * This class represents a status for saving a game, including the game itself and the file path
+ * where the game should be saved. It provides methods to serialize game data to JSON format and save
+ * it to a file.
+ */
 public class SaveGameStatus {
-    private Game game;
-    private String filePath;
+    private final Game game; //The game object to be saved.
+    private final String filePath; //The file path where the game should be saved.
 
+    /**
+     * Constructs a new `SaveGameStatus` object with the specified `game` and `filePath`.
+     * @param game the game object to be saved
+     * @param filePath the file path where the game should be saved
+     */
     public SaveGameStatus(Game game, String filePath) {
         this.game = game;
         this.filePath = filePath;
     }
 
-    // function to put all the information relative to the player into a JSON
+    /**
+     * Generates a JSON object containing all the information about a given player.
+     * @param player the player to generate JSON data for
+     * @return a JSON object containing all the information about the given player
+     */
     public JsonObject jsonPlayer(Player player) {
         Gson gson = new Gson();
         JsonObject json = new JsonObject();
@@ -28,9 +40,13 @@ public class SaveGameStatus {
         json.add("personalGoal", gson.toJsonTree(player.getPersonalGoal()));
 
         return json;
-    };
+    }
 
-    // function to put all the information relative to the board into a JSON
+    /**
+     * Generates a JSON object containing all the information about a given board.
+     * @param board the board to generate JSON data for
+     * @return a JSON object containing all the information about the given board
+     */
     public JsonObject jsonBoard(Board board) {
         Gson gson = new Gson();
         JsonObject json = new JsonObject();
@@ -41,7 +57,11 @@ public class SaveGameStatus {
         return json;
     }
 
-    // function to put all the information relative to the commonGoal into a JSON
+    /**
+     * Generates a JSON object containing all the information about a given common goal.
+     * @param commonGoal the common goal to generate JSON data for
+     * @return a JSON object containing all the information about the given common goal
+     */
     public JsonObject jsonCommonGoal(CommonGoal commonGoal) {
         JsonObject json = new JsonObject();
         json.add("goalCode", new JsonPrimitive(commonGoal.getGoalCode()));
@@ -50,7 +70,10 @@ public class SaveGameStatus {
         return json;
     }
 
-    // function to create a JSON that contains all the information relative to all players
+    /**
+     * Generates a JSON object containing all the information about all players in the game.
+     * @return a JSON object containing all the information about all players in the game
+     */
     public JsonObject jsonPlayerArray() {
         Gson gsonTmp = new Gson();
         JsonObject jsonTmp = new JsonObject();
@@ -60,7 +83,10 @@ public class SaveGameStatus {
         return jsonTmp;
     }
 
-    // function to create a JSON that contains all the information relative to all commonGoals
+    /**
+     * Generates a JSON object containing all the information about all common goals in the game.
+     * @return a JSON object containing all the information about all common goals in the game
+     */
     public JsonObject jsonCommonGoalArray() {
         Gson gsonTmp = new Gson();
         JsonObject jsonTmp = new JsonObject();
@@ -70,11 +96,13 @@ public class SaveGameStatus {
         return jsonTmp;
     }
 
-    // function that save a JSON object containing all the model status information in a file
+    /**
+     * Serializes the game data to JSON format and saves it to a file.
+     */
     public void saveGame() {
-        // creazione dell'oggetto Gson con la formattazione indentata
+        // creation of the object Gson with indented format
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        // serializzazione del modello in formato JSON
+        //JSON serialization of the object
         JsonObject json = new JsonObject();
 
         json.add("firstPlayer", new JsonPrimitive(game.getFirstPlayer().getNickname()));
@@ -83,9 +111,7 @@ public class SaveGameStatus {
         json.add("board", gson.toJsonTree(jsonBoard(game.getBoard())));
         json.add("commonGoals", gson.toJsonTree(jsonCommonGoalArray()));
 
-
-
-        // scrittura del JSON su un file
+        //write the json opn a file
         try (FileWriter fileWriter = new FileWriter(filePath)) {
             gson.toJson(json, fileWriter);
         } catch (IOException e) {
