34235d857169be8478935162fb8f18b30f65262e
Davide Simoncini <simondade@gmail.com>
2023-03-23 17:03:06 +0100

    Fixed the personalGoal constructor exceptions

--- src/test/java/org/projectsw/ModelTest/PersonalGoalTest.java
+++ src/test/java/org/projectsw/ModelTest/PersonalGoalTest.java
@@ -3,33 +3,29 @@ package org.projectsw.ModelTest;
 import org.junit.jupiter.api.Test;
 import org.projectsw.Model.PersonalGoal;
 import org.projectsw.Model.TilesEnum;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class PersonalGoalTest {
 
     //test that the method returns the right personalGoal
     @Test
-    void getPersonalGoal() throws IOException {
+    void getPersonalGoal(){
         PersonalGoal personalGoal = new PersonalGoal(0);
         PersonalGoal.cleanUsedCodes();
-        assertTrue(
-        personalGoal.getPersonalGoal()[3][1].equals(TilesEnum.GAMES)&&
-        personalGoal.getPersonalGoal()[0][0].equals(TilesEnum.PLANTS)&&
-        personalGoal.getPersonalGoal()[0][2].equals(TilesEnum.FRAMES)&&
-        personalGoal.getPersonalGoal()[1][4].equals(TilesEnum.CATS)&&
-        personalGoal.getPersonalGoal()[2][3].equals(TilesEnum.BOOKS)&&
-        personalGoal.getPersonalGoal()[5][2].equals(TilesEnum.TROPHIES));
+        assertTrue(personalGoal.getPersonalGoal()[3][1].equals(TilesEnum.GAMES)&&
+                    personalGoal.getPersonalGoal()[0][0].equals(TilesEnum.PLANTS)&&
+                    personalGoal.getPersonalGoal()[0][2].equals(TilesEnum.FRAMES)&&
+                    personalGoal.getPersonalGoal()[1][4].equals(TilesEnum.CATS)&&
+                    personalGoal.getPersonalGoal()[2][3].equals(TilesEnum.BOOKS)&&
+                    personalGoal.getPersonalGoal()[5][2].equals(TilesEnum.TROPHIES));
     }
 
     //test that the constructor returns a correct and full maxtrix of tiles
     //tested for all 12 codes
     @Test
-    void integrityTest() throws IOException {
+    void integrityTest(){
         for(int i=0;i<12;i++){
             PersonalGoal personalGoal = new PersonalGoal(i);
             for(int j=0;j<6;j++){
