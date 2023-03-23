8d841d996683bfe9019c88291b5805bbde513373
Lorenzo Luisi <116544919+Lorenzo-Luisi@users.noreply.github.com>
2023-03-23 17:30:41 +0100

    added some javadoc to commonGoals classes

--- src/main/java/org/projectsw/Model/CommonGoal/CommonGoal.java
+++ src/main/java/org/projectsw/Model/CommonGoal/CommonGoal.java
@@ -9,16 +9,28 @@ public abstract class CommonGoal{
         this.redeemedNumber = 0;
     }
 
+    /**
+     * @return the redeemedNumber that shows how many times a specific CommonGoal has been redeemed
+     */
     public int getRedeemedNumber(){
         return this.redeemedNumber;
     }
 
+    /**
+     * increase the redeemedNumber that shows how many times a specific CommonGoal has been redeemed
+     * @throws MaximumRedeemedPointsException
+     */
     public void increaseRedeemedNumber() throws MaximumRedeemedPointsException{
         if(getRedeemedNumber()<4)
             redeemedNumber++;
         else throw  new MaximumRedeemedPointsException("There are no more points to redeem");
     }
 
+    /**
+     * @param shelf is the player's shelf
+     * @return true after the override by the subclasses if the player's shelf match the requirements of the
+     * specific commonGoal, false otherwise
+     */
     public boolean check(Shelf shelf){
         return false;
     }
