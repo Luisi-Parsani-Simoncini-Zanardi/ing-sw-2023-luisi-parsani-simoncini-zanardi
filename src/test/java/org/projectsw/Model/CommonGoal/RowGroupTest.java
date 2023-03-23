ba8eb71c4706b6c5cbc1c4894200726259af3b13
Lorenzo Luisi <116544919+Lorenzo-Luisi@users.noreply.github.com>
2023-03-23 02:22:17 +0100

    updated the RowGroupTest Test class

--- src/test/java/org/projectsw/Model/CommonGoal/RowGroupTest.java
+++ src/test/java/org/projectsw/Model/CommonGoal/RowGroupTest.java
@@ -7,18 +7,37 @@ import org.projectsw.Model.TilesEnum;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class RowTest {
+class RowGroupTest {
 
+    //tests that the algorithm successfully checks that the shelf meets the requirements of the chosen CommonGoal
     @Test
-    void check() {
+    void checkTrue() {
         CommonGoal commonRow = new Row(new RowGroup());
         Shelf shelf = new Shelf();
-        Tile tile = new Tile(TilesEnum.CATS,0);
         for(int i=0; i<6; i++)
-            for(int j=0;j<5;j++)
-                try {
-                    shelf.insertTiles(tile, i, j);
-                }catch(Exception e){}
+            try {
+                shelf.insertTiles(new Tile(TilesEnum.CATS,0), i, 0);
+                shelf.insertTiles(new Tile(TilesEnum.CATS,0), i, 1);
+                shelf.insertTiles(new Tile(TilesEnum.CATS,0), i, 2);
+                shelf.insertTiles(new Tile(TilesEnum.FRAMES,0), i, 3);
+                shelf.insertTiles(new Tile(TilesEnum.GAMES,0), i, 4);
+            }catch(Exception e){}
         assertTrue(commonRow.check(shelf));
+    }
+
+    //tests that the algorithm successfully checks that the shelf doesn't meet the requirements of the chosen CommonGoal
+    @Test
+    void checkFalse() {
+        CommonGoal commonRow = new Row(new RowGroup());
+        Shelf shelf = new Shelf();
+        for(int i=0; i<6; i++)
+            try {
+                shelf.insertTiles(new Tile(TilesEnum.CATS,0), i, 0);
+                shelf.insertTiles(new Tile(TilesEnum.CATS,0), i, 1);
+                shelf.insertTiles(new Tile(TilesEnum.BOOKS,0), i, 2);
+                shelf.insertTiles(new Tile(TilesEnum.FRAMES,0), i, 3);
+                shelf.insertTiles(new Tile(TilesEnum.GAMES,0), i, 4);
+            }catch(Exception e){}
+        assertFalse(commonRow.check(shelf));
     }
 }
