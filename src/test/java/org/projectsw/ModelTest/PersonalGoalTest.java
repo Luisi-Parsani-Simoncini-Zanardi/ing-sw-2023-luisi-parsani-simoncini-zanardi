ced97fb862830894f35e29101495784fcdba91b6
Davide Simoncini <simondade@gmail.com>
2023-03-24 14:25:28 +0100

    Added new empty constructor to personalGoal, also tested it, and upgraded some tests to personalGoalTest

--- src/test/java/org/projectsw/ModelTest/PersonalGoalTest.java
+++ src/test/java/org/projectsw/ModelTest/PersonalGoalTest.java
@@ -1,5 +1,6 @@
 package org.projectsw.ModelTest;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.projectsw.Model.PersonalGoal;
 import org.projectsw.Model.TilesEnum;
@@ -10,41 +11,79 @@ import static org.junit.jupiter.api.Assertions.*;
 class PersonalGoalTest {
 
     /**
-     * tests if the method returns the correct personalGoal
+     * Clean the list of used codes before each test
+     */
+    @BeforeEach
+    void codesCleaner(){
+        PersonalGoal.cleanUsedCodes();
+    }
+
+    /**
+     * Test if the empty constructor returns a correct matrix of EMPTY tiles
+     */
+    @Test
+    void emptyIntegrityTest(){
+        PersonalGoal personalGoal = new PersonalGoal();
+        assertEquals(6,personalGoal.getPersonalGoal().length);
+        assertEquals(5,personalGoal.getPersonalGoal()[0].length);
+        for(int j=0;j<6;j++){
+            for(int h=0;h<5;h++){
+                assertEquals(personalGoal.getPersonalGoal()[j][h], TilesEnum.EMPTY);
+            }
+        }
+    }
+
+    /**
+     * Test if the constructor returns a correct and full matrix of tiles
+     */
+    @Test
+    void integrityTest(){
+        for(int i=0;i<12;i++){
+            int cats = 0, books = 0, games = 0, frames = 0, plants = 0, trophies = 0, empty = 0;
+            PersonalGoal personalGoal = new PersonalGoal(i);
+            assertEquals(6,personalGoal.getPersonalGoal().length);
+            assertEquals(5,personalGoal.getPersonalGoal()[0].length);
+            for(int j=0;j<6;j++){
+                for(int h=0;h<5;h++){
+                    TilesEnum tile = personalGoal.getPersonalGoal()[j][h];
+                    if(tile == TilesEnum.CATS) cats++;
+                    if(tile == TilesEnum.FRAMES) frames++;
+                    if(tile == TilesEnum.GAMES) games++;
+                    if(tile == TilesEnum.TROPHIES) trophies++;
+                    if(tile == TilesEnum.PLANTS) plants++;
+                    if(tile == TilesEnum.BOOKS) books++;
+                    if(tile == TilesEnum.EMPTY) empty++;
+                    assertTrue(tile.equals(TilesEnum.CATS)||
+                            tile.equals(TilesEnum.FRAMES)||
+                            tile.equals(TilesEnum.BOOKS)||
+                            tile.equals(TilesEnum.GAMES)||
+                            tile.equals(TilesEnum.TROPHIES)||
+                            tile.equals(TilesEnum.PLANTS)||
+                            tile.equals(TilesEnum.EMPTY));
+                }
+            }
+            assertEquals(1,cats);
+            assertEquals(1,frames);
+            assertEquals(1,games);
+            assertEquals(1,trophies);
+            assertEquals(1,plants);
+            assertEquals(1,books);
+            assertEquals(24,empty);
+        }
+    }
+
+    /**
+     * Tests if the returned personalGoal the correct one
      */
     @Test
     void testGetPersonalGoal(){
         PersonalGoal personalGoal = new PersonalGoal(0);
-        PersonalGoal.cleanUsedCodes();
         assertTrue(personalGoal.getPersonalGoal()[3][1].equals(TilesEnum.GAMES)&&
                     personalGoal.getPersonalGoal()[0][0].equals(TilesEnum.PLANTS)&&
                     personalGoal.getPersonalGoal()[0][2].equals(TilesEnum.FRAMES)&&
                     personalGoal.getPersonalGoal()[1][4].equals(TilesEnum.CATS)&&
                     personalGoal.getPersonalGoal()[2][3].equals(TilesEnum.BOOKS)&&
                     personalGoal.getPersonalGoal()[5][2].equals(TilesEnum.TROPHIES));
-    }
-
-    /**
-     * test if the constructor returns a correct and full matrix of tiles
-     */
-    @Test
-    void integrityTest(){
-        for(int i=0;i<12;i++){
-            PersonalGoal personalGoal = new PersonalGoal(i);
-            for(int j=0;j<6;j++){
-                for(int h=0;h<5;h++){
-                    TilesEnum tile = personalGoal.getPersonalGoal()[j][h];
-                    assertTrue(tile.equals(TilesEnum.CATS)||
-                                tile.equals(TilesEnum.FRAMES)||
-                                tile.equals(TilesEnum.BOOKS)||
-                                tile.equals(TilesEnum.GAMES)||
-                                tile.equals(TilesEnum.TROPHIES)||
-                                tile.equals(TilesEnum.PLANTS)||
-                                tile.equals(TilesEnum.EMPTY));
-                }
-            }
-        }
-        PersonalGoal.cleanUsedCodes();
     }
 
     /**
