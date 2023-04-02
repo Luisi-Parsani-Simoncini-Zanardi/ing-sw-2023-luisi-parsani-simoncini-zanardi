8083ed4c4a87084f1677d139fa2faa67e52d9e0c
Lorenzo Luisi <116544919+Lorenzo-Luisi@users.noreply.github.com>
2023-04-02 16:17:57 +0200

    fixed TriangleTest

--- src/test/java/org/projectsw/ModelTest/CommonGoalTest/TriangleTest.java
+++ src/test/java/org/projectsw/ModelTest/CommonGoalTest/TriangleTest.java
@@ -2,6 +2,7 @@ package org.projectsw.ModelTest.CommonGoalTest;
 
 import org.junit.jupiter.api.Test;
 import org.projectsw.Model.CommonGoal.CommonGoal;
+import org.projectsw.Model.CommonGoal.CommonGoalStrategy;
 import org.projectsw.Model.CommonGoal.Triangle;
 import org.projectsw.Model.Shelf;
 import org.projectsw.Model.Tile;
@@ -17,46 +18,47 @@ class TriangleTest {
      */
     @Test
     void checkTrue() {
-        CommonGoal triangle = new Triangle();
+        CommonGoalStrategy strategy = new Triangle();
+        CommonGoal triangle = new CommonGoal(strategy);
         Shelf shelf1 = new Shelf();
         for(int i=0; i<5; i++) {
             for (int j=5; i < j ; j--){
                 try {
                     shelf1.insertTiles(new Tile(TilesEnum.CATS,0), j, i);
-                } catch (Exception e) {}
+                } catch (Exception ignored) {}
             }
         }
-        assertTrue(triangle.check(shelf1));
+        assertTrue(triangle.checkRequirements(shelf1));
 
         Shelf shelf2 = new Shelf();
         for(int i=0; i<5; i++) {
             for (int j=5; i-1 < j ; j--){
                 try {
                     shelf2.insertTiles(new Tile(TilesEnum.CATS,0), j, i);
-                } catch (Exception e) {}
+                } catch (Exception ignored) {}
             }
         }
-        assertTrue(triangle.check(shelf2));
+        assertTrue(triangle.checkRequirements(shelf2));
 
         Shelf shelf3 = new Shelf();
         for(int i=0; i<5; i++) {
             for (int j=5; j > 4-i ; j--){
                 try {
                     shelf3.insertTiles(new Tile(TilesEnum.CATS,0), j, i);
-                } catch (Exception e) {}
+                } catch (Exception ignored) {}
             }
         }
-        assertTrue(triangle.check(shelf3));
+        assertTrue(triangle.checkRequirements(shelf3));
 
         Shelf shelf4 = new Shelf();
         for(int i=0; i<5; i++) {
             for (int j=5; j > 3-i ; j--){
                 try {
                     shelf4.insertTiles(new Tile(TilesEnum.CATS,0), j, i);
-                } catch (Exception e) {}
+                } catch (Exception ignored) {}
             }
         }
-        assertTrue(triangle.check(shelf4));
+        assertTrue(triangle.checkRequirements(shelf4));
     }
 
     /**
@@ -64,18 +66,19 @@ class TriangleTest {
      */
     @Test
     void checkFalse() {
-        CommonGoal triangle = new Triangle();
+        CommonGoalStrategy strategy = new Triangle();
+        CommonGoal triangle = new CommonGoal(strategy);
         Shelf shelf1 = new Shelf();
         for(int i=0; i<5; i++) {
             for (int j=5; i < j ; j--){
                 try {
                     shelf1.insertTiles(new Tile(TilesEnum.CATS,0), j, i);
-                } catch (Exception e) {}
+                } catch (Exception ignored) {}
             }
         }
         try {
             shelf1.insertTiles(new Tile(TilesEnum.CATS, 0), 4, 4);
-        }catch(Exception e){}
-        assertFalse(triangle.check(shelf1));
+        }catch(Exception ignored){}
+        assertFalse(triangle.checkRequirements(shelf1));
     }
 }
