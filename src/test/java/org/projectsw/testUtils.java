e74f099905970ba5b32cc7e9b993464abf5084a0
Luca Parsani <luca@parsani.it>
2023-04-05 11:33:15 +0200

    fixed javadocs

--- src/test/java/org/projectsw/testUtils.java
+++ src/test/java/org/projectsw/testUtils.java
@@ -85,8 +85,8 @@ public class testUtils {
 
     /**
      * check if two Message objects are identical
-     * @param messageTest
-     * @param messageAssert
+     * @param messageTest a test Message object
+     * @param messageAssert another test Message object
      */
     public void assertEqualsMessage (Message messageTest, Message messageAssert) {
         assertEquals(messageTest.getContent(), messageAssert.getContent());
