f41d5800065ad02eae8c60cdabbbc23b58188465
Lorenzo Luisi <116544919+Lorenzo-Luisi@users.noreply.github.com>
2023-04-06 01:02:51 +0200

    The Groups.java algorithm has been fixed and its tests written

--- src/test/java/org/projectsw/ModelTest/CommonGoalTest/GroupsTest.java
+++ src/test/java/org/projectsw/ModelTest/CommonGoalTest/GroupsTest.java
@@ -12,8 +12,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GroupsTest {
 
+    /**
+     * Check if there are at least 4 groups of size 4 of tiles of the same type
+     */
     @Test
-    void trueGroupOfFour() {
+    void trueFourGroupOfFour() {
         CommonGoalStrategy strategy = new Groups(3);
         CommonGoal common = new CommonGoal(strategy);
         Shelf shelf = new Shelf();
@@ -47,5 +50,74 @@ class GroupsTest {
         catch(Exception ignore){}
 
         assertTrue(common.checkRequirements(shelf));
+    }
+
+    /**
+     * Checks that groups composed of empty tiles are not counted
+     */
+    @Test
+    void emptyFourTilesShelf(){
+        CommonGoalStrategy strategy = new Groups(3);
+        CommonGoal common = new CommonGoal(strategy);
+        Shelf shelf = new Shelf();
+
+        try{
+            shelf.insertTiles(new Tile(TilesEnum.CATS, 0), 0,0);
+            shelf.insertTiles(new Tile(TilesEnum.CATS, 0), 1,0);
+            shelf.insertTiles(new Tile(TilesEnum.CATS, 0), 1,1);
+            shelf.insertTiles(new Tile(TilesEnum.CATS, 0), 2,1);
+
+            shelf.insertTiles(new Tile(TilesEnum.BOOKS, 0), 3,0);
+            shelf.insertTiles(new Tile(TilesEnum.BOOKS, 0), 4,0);
+            shelf.insertTiles(new Tile(TilesEnum.BOOKS, 0), 5,0);
+            shelf.insertTiles(new Tile(TilesEnum.BOOKS, 0), 5,1);
+
+            shelf.insertTiles(new Tile(TilesEnum.FRAMES, 0), 0,4);
+            shelf.insertTiles(new Tile(TilesEnum.FRAMES, 0), 1,4);
+            shelf.insertTiles(new Tile(TilesEnum.FRAMES, 0), 1,3);
+            shelf.insertTiles(new Tile(TilesEnum.FRAMES, 0), 2,3);
+        }
+        catch(Exception ignore){}
+
+        assertFalse(common.checkRequirements(shelf));
+    }
+
+    /**
+     * Checks that the empty shelf does not contain any groups
+     */
+    @Test
+    void emptyFourShelf(){
+        CommonGoalStrategy strategy = new Groups(3);
+        CommonGoal common = new CommonGoal(strategy);
+        Shelf shelf = new Shelf();
+
+        assertFalse(common.checkRequirements(shelf));
+    }
+
+    /**
+     * Checks that the shelf does not contain at least 4 groups made up of 4 tiles of the same type
+     */
+    @Test
+    void falseFourShelf(){
+        CommonGoalStrategy strategy = new Groups(3);
+        CommonGoal common = new CommonGoal(strategy);
+        Shelf shelf = new Shelf();
+
+        try{
+            for(int i=0; i<2; i++)
+                for(int j=0; j<5; j++)
+                    shelf.insertTiles(new Tile(TilesEnum.FRAMES, 0), i,j);
+
+            for(int i=2; i<4; i++)
+                for(int j=0; j<5; j++)
+                    shelf.insertTiles(new Tile(TilesEnum.BOOKS, 0), i,j);
+
+            for(int i=4; i<6; i++)
+                for(int j=0; j<5; j++)
+                    shelf.insertTiles(new Tile(TilesEnum.CATS, 0), i,j);
+        }
+        catch(Exception ignore){}
+
+        assertFalse(common.checkRequirements(shelf));
     }
 }
