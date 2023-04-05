6fca65f1ca3789474f7e9e218d8510b4f7199fae
Davide Simoncini <simondade@gmail.com>
2023-04-05 13:58:52 +0200

    Added the new methods for the start of the game and modified some others

--- src/main/java/org/projectsw/Controller/Engine.java
+++ src/main/java/org/projectsw/Controller/Engine.java
@@ -18,33 +18,48 @@ public class Engine {
     public Game getGame() { return this.game; }
 
     //TODO: finire metodi controller
+
     /**
-     * Initializes the game and his save.
+     * Sets the game status to RUNNING, saves the first instance of the game and lunch the first turn.
      */
     public void startGame(){
-        this.game = new Game();
-        SaveGameStatus saveGameStatus = new SaveGameStatus(game, "");
-        //TODO: !!!POST!!! aggiungere filepath
+        game.setGameState(GameStates.RUNNING);
+        SaveGameStatus saveGameStatus = new SaveGameStatus(game, "");//TODO: !!!POST!!! aggiungere filepath
+        //chiama il metodo di inizio turno
     }
 
     /**
-     * Creates a player object and adds it to the array of players if there's enough room.
-     * If the player is the first to join, also sets him as first and current player
+     * Creates a player object with position 0 and create a new game using the game constructor (the one that also sets the first player)
+     * the game is initialized using the first player and the number of players selected, the state of the game at the end of the
+     * execution is LOBBY.
+     * @param nicknameFirstPlayer the nickname of the first player joining in the game
+     * @param numberOfPlayers the number of players selected by the first player
+     */
+    public void firstPlayerJoin(String nicknameFirstPlayer, int numberOfPlayers){
+        Player firstPlayer = new Player(nicknameFirstPlayer,0);
+        game = new Game(firstPlayer,numberOfPlayers);
+    }
+
+    /**
+     * If the game state isn't LOBBY the join request is negated. If the game state is LOBBY it creates a new
+     * player object with the right position checking if the lobby is fulled: if it is, calls the method to start the game,
+     * if it isn't the game state remains LOBBY, waiting for new join requests.
      * @param nickname the nickname of the player to be created
      */
     public void playerJoin (String nickname){
-        int playerLength = game.getPlayers().size();
-        Player player = new Player(nickname, playerLength+1);
-        if (playerLength==0){
-            game.setFirstPlayer(player);
-            game.setCurrentPlayer(player);
-            //TODO: !!!POST!!! aggiungere funzione che chiede al player quanti giocatori ci sono
+        if(game.getGameState().equals(GameStates.LOBBY)){
+            try {
+                int newPlayerPosition = game.getPlayers().size();
+                Player newPlayer = new Player(nickname,newPlayerPosition);
+                game.addPlayer(newPlayer);
+                if (game.getPlayers().size() == game.getNumberOfPlayers()) {
+                    startGame();
+                }
+            } catch (InvalidNameException e) {
+                System.out.println("Player join failed, the nickname is already used");
+            }
         }
-        try {
-            game.addPlayer(player);
-        } catch (InvalidNameException e) {
-            //send message
-        }
+        else System.out.println("Player join failed, the game isn't in the lobby state");
     }
 
     //select from 1 to 3 adjacent tiles and with a free side, and put them in temporarytiles in the Player class
