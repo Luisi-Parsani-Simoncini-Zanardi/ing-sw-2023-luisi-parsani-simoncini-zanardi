0b9d425a3d2354612a3fe139cbaa0e916a8ffa5d
Lorenzo Luisi <116544919+Lorenzo-Luisi@users.noreply.github.com>
2023-04-28 18:07:58 +0200

    created a register for the clients in the ServerImpl

--- src/main/java/org/projectsw/Distributed/ServerImpl.java
+++ src/main/java/org/projectsw/Distributed/ServerImpl.java
@@ -1,10 +1,15 @@
 package org.projectsw.Distributed;
 
 import org.projectsw.Controller.Engine;
+import org.projectsw.Exceptions.FirstJoinFailedException;
+import org.projectsw.Exceptions.JoinFailedException;
 import org.projectsw.Model.Game;
 import org.projectsw.Model.GameView;
 
 import java.rmi.RemoteException;
+import java.util.Scanner;
+
+import static org.projectsw.Config.maxPlayers;
 
 public class ServerImpl implements Server{
 
@@ -12,6 +17,23 @@ public class ServerImpl implements Server{
 
     @Override
     public void register(Client client) throws RemoteException {
+        //TODO: qui si dovrà gestire la possibile reconnect se un savegame è presente
+        if(this.controller.getClients().size()==0){
+            String nickname = insertNickname();
+            int num = insertNumOfPlayers();
+            try {
+                this.controller.firstPlayerJoin(nickname, num);
+            } catch (FirstJoinFailedException e) {
+                throw new RuntimeException(e);
+            }
+        }else if(this.controller.getClients().size()<maxPlayers){
+            String nickname = insertNickname();
+            try {
+                this.controller.playerJoin(nickname);
+            } catch (JoinFailedException e) {
+                throw new RuntimeException(e);
+            }
+        }
         this.controller.getClients().add(client);
         //adding observer
         this.controller.getGame().addObserver((o, arg) -> {
@@ -21,6 +43,18 @@ public class ServerImpl implements Server{
                 throw new RuntimeException(e);
             }
         });
+    }
+
+    private String insertNickname(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("insert your nickname: ");
+        return scanner.nextLine();
+    }
+
+    private int insertNumOfPlayers(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("insert the number of players: ");
+        return scanner.nextInt();
     }
 
     @Override
