cc7aa78176e16101dba223fbcf6f77d3cc47f37c
Davide Simoncini <simondade@gmail.com>
2023-06-15 14:22:12 +0200

    Added all images to resources and improved path solver

--- src/main/java/org/projectsw/Util/PathSolverGui.java
+++ src/main/java/org/projectsw/Util/PathSolverGui.java
@@ -54,4 +54,83 @@ public class PathSolverGui {
         return standardInitialPath+"/PersonalGoals/empty"+formatJpg;
     }
 
+    public static String boxLogo() {
+        return standardInitialPath+"LogoAndIcons/Box280x280"+formatPng;
+    }
+
+    public static String icon() {
+        return standardInitialPath+"LogoAndIcons/Icon"+formatPng;
+    }
+
+    public static String publisher() {
+        return standardInitialPath+"LogoAndIcons/Publisher"+formatPng;
+    }
+
+    public static String title618() {
+        return standardInitialPath+"LogoAndIcons/Title2000x618"+formatPng;
+    }
+
+    public static String title2000() {
+        return standardInitialPath+"LogoAndIcons/Title2000x2000"+formatPng;
+    }
+
+    public static String endGameToken() {
+        return standardInitialPath+"ScoringTokens/EndGame"+formatJpg;
+    }
+
+    public static String ScoringToken2() {
+        return standardInitialPath+"ScoringTokens/Scoring2"+formatJpg;
+    }
+
+    public static String ScoringToken4() {
+        return standardInitialPath+"ScoringTokens/Scoring4"+formatJpg;
+    }
+
+    public static String ScoringToken6() {
+        return standardInitialPath+"ScoringTokens/Scoring6"+formatJpg;
+    }
+
+    public static String ScoringToken8() {
+        return standardInitialPath+"ScoringTokens/Scoring8"+formatJpg;
+    }
+
+    public static String ScoringTokenEmpty1() {
+        return standardInitialPath+"ScoringTokens/ScoringEmpty1"+formatJpg;
+    }
+
+    public static String ScoringTokenEmpty2() {
+        return standardInitialPath+"ScoringTokens/ScoringEmpty2"+formatJpg;
+    }
+
+    public static String ScoringTokenBack() {
+        return standardInitialPath+"ScoringTokens/ScoringBack"+formatJpg;
+    }
+
+    public static String FirstPlayerToken() {
+        return standardInitialPath+"Various/FirstPlayerToken"+formatPng;
+    }
+
+    public static String Books(int code){
+        return standardInitialPath+"Tiles/Books"+code+formatPng;
+    }
+
+    public static String Cats(int code){
+        return standardInitialPath+"Tiles/Cats"+code+formatPng;
+    }
+
+    public static String Frames(int code){
+        return standardInitialPath+"Tiles/Frames"+code+formatPng;
+    }
+
+    public static String Games(int code){
+        return standardInitialPath+"Tiles/Games"+code+formatPng;
+    }
+
+    public static String Plants(int code){
+        return standardInitialPath+"Tiles/Plants"+code+formatPng;
+    }
+
+    public static String Trophies(int code){
+        return standardInitialPath+"Tiles/Trophies"+code+formatPng;
+    }
 }
---
+++ src/main/java/org/projectsw/View/GraphicalUI/MessagesGUI/MessageFrame.java
@@ -0,0 +1,9 @@
+package org.projectsw.View.GraphicalUI.MessagesGUI;
+
+import javax.swing.*;
+
+public class MessageFrame extends JFrame {
+    public MessageFrame() {
+        setLocationRelativeTo(null);
+    }
+}
