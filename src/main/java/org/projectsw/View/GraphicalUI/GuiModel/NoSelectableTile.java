bc9ade0e2e41fe74734bbb95de7c91e1523a2aa3
lorenzoZanardi <lorenzo.zanardi@mail.polimi.it>
2023-06-30 11:43:06 +0200

    tiles

--- src/main/java/org/projectsw/View/GraphicalUI/GuiModel/NoSelectableTile.java
+++ src/main/java/org/projectsw/View/GraphicalUI/GuiModel/NoSelectableTile.java
@@ -4,9 +4,13 @@ import org.projectsw.Model.Enums.TilesEnum;
 import org.projectsw.Model.Tile;
 import org.projectsw.Util.PathSolverGui;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 /*
  * This class represents the JLabel equivalent of the tile.
@@ -19,16 +23,30 @@ public class NoSelectableTile extends JLabel {
      */
     public NoSelectableTile(Tile tile) {
         super();
-        ImageIcon icon = null;
+        String path;
         int size = 50;
         switch (tile.getTile()) {
-            case CATS -> icon = new ImageIcon(PathSolverGui.cats(tile.getImageNumber()));
-            case GAMES -> icon = new ImageIcon(PathSolverGui.games(tile.getImageNumber()));
-            case FRAMES -> icon = new ImageIcon(PathSolverGui.frames(tile.getImageNumber()));
-            case PLANTS -> icon = new ImageIcon(PathSolverGui.plants(tile.getImageNumber()));
-            case TROPHIES -> icon = new ImageIcon(PathSolverGui.trophies(tile.getImageNumber()));
-            case BOOKS -> icon = new ImageIcon(PathSolverGui.books(tile.getImageNumber()));
+            case CATS -> path = "/ImagesGui/Tiles/Cats" + tile.getImageNumber() + ".png";
+            case GAMES -> path = "/ImagesGui/Tiles/Games" + tile.getImageNumber() + ".png";
+            case FRAMES -> path = "/ImagesGui/Tiles/Frames" + tile.getImageNumber() + ".png";
+            case PLANTS -> path = "/ImagesGui/Tiles/Plants" + tile.getImageNumber() + ".png";
+            case TROPHIES -> path = "/ImagesGui/Tiles/Trophies" + tile.getImageNumber() + ".png";
+            case BOOKS -> path = "/ImagesGui/Tiles/Books" + tile.getImageNumber() + ".png";
+            default -> path = "/ImagesGui/Tiles/Books0.png";
         }
+
+        InputStream inputStream = NoSelectableTile.class.getResourceAsStream(path);
+
+        BufferedImage image1 = null;
+
+        try {
+            image1 = ImageIO.read(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        ImageIcon icon = new ImageIcon(image1);
+
         Border separatorBorder = BorderFactory.createMatteBorder(1, 1, 0, 0, Color.BLACK);
         setPreferredSize(new Dimension(size,size));
         setBorder(separatorBorder);
